69efa20aa6f605895a80addcfb09e00a72dcd6a3
rafaelwsilva <rws1602@gmail.com>
2014-12-01 12:23:12 -0200

    Update insertPoll

--- Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/service/QuestionService.java
+++ Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/service/QuestionService.java
@@ -2,27 +2,35 @@ package br.edu.univas.lab6.smartpoll.service;
 
 import java.util.List;
 
+import br.edu.univas.lab6.smartpoll.dao.AnswerDAO;
 import br.edu.univas.lab6.smartpoll.dao.QuestionDAO;
+import br.edu.univas.lab6.smartpoll.entity.Answer;
 import br.edu.univas.lab6.smartpoll.entity.Question;
 import br.edu.univas.lab6.smartpoll.managers.SimpleEntityManager;
 
 public class QuestionService {
 
 	private QuestionDAO dao;
+	private AnswerDAO answerDAO;
 
 	private SimpleEntityManager simpleEntityManager;
 
 	public QuestionService(SimpleEntityManager simpleEntityManager) {
 		this.simpleEntityManager = simpleEntityManager;
 		dao = new QuestionDAO(simpleEntityManager.getEntityManager());
+		answerDAO = new AnswerDAO(simpleEntityManager.getEntityManager());
 	}
 
 	public void save(Question question) {
 		try {
 			simpleEntityManager.beginTransaction();
 			dao.save(question);
+			for (Answer answer : question.getAnswers()) {
+				answer.setQuestion(question);
+				answerDAO.save(answer);
+			}
 			simpleEntityManager.commit();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			simpleEntityManager.rollBack();
