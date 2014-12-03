d97ff260a1741fa2bd2be8e9c7109930a51f981e
ThiagoScodeler <tscodeler@gmail.com>
2014-12-03 19:05:22 -0200

    Update JSF Message

--- Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/util/JSFMessage.java
+++ Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/util/JSFMessage.java
@@ -7,9 +7,13 @@ import javax.faces.context.FacesContext;
 public class JSFMessage {
 	
 
-
 	public void sendErrorMessageToUser(String message) {
 		FacesMessage facesMessage = createMessage(FacesMessage.SEVERITY_ERROR, message);
+		addMessageToJsfContext(facesMessage);
+	}
+	
+	public void sendInfoMessageToUser(String message) {
+		FacesMessage facesMessage = createMessage(FacesMessage.SEVERITY_INFO, message);
 		addMessageToJsfContext(facesMessage);
 	}
 
