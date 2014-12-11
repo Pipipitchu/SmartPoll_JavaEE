5e8bbb120ebe1f05931fbcfac5058f81fb0242d6
rafaelwsilva <rws1602@gmail.com>
2014-12-11 13:16:35 -0200

    Update Dashboard

--- Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/main/MainClass.java
+++ Smartpoll/src/main/java/br/edu/univas/lab6/smartpoll/main/MainClass.java
@@ -1,32 +1,50 @@
 package br.edu.univas.lab6.smartpoll.main;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-
-import br.edu.univas.lab6.smartpoll.entity.Question;
-import br.edu.univas.lab6.smartpoll.managers.SimpleEntityManager;
-import br.edu.univas.lab6.smartpoll.service.QuestionService;
+import java.util.Random;
 
 public class MainClass {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 
-		List<Question> questions = new ArrayList<Question>();
+		Integer amountOfVotes = 200;
 
-		SimpleEntityManager simpleEntityManager = new SimpleEntityManager();
+		Integer answer = 0;
+		Integer question = 0;
+		Integer month = 0;
+		Integer day = 0;
 
-		QuestionService questionService = new QuestionService(
-				simpleEntityManager);
+		Random random = new Random();
 
-		questions = questionService.findByMonthVote(11);
+		for (int i = 0; i < amountOfVotes; i++) {
 
-		System.out.println(questions.size());
-		
-		for (Question question : questions) {
-			System.out.println(question.getTitle());
-			System.out.println(question.getResults().size());
+			question = random.nextInt(2) + 28;
 
+			if (question == 28) {
+				answer = random.nextInt(4) + 25;
+			} else {
+				answer = random.nextInt(4) + 29;
+			}
+
+			month = random.nextInt(12) + 1;
+
+			if (month == 2) {
+				day = random.nextInt(28) + 1;
+			} else if (month == 1 || month == 3 || month == 5 || month == 7
+					|| month == 8 || month == 10) {
+				day = random.nextInt(31) + 1;
+			} else if (month == 12) {
+				day = random.nextInt(11) + 1;
+			} else {
+				day = random.nextInt(30) + 1;
+			}
+
+			String sql = "insert into result (question_id, answer_id, date_vote) values(";
+			sql += question + ", ";
+			sql += answer + ", ";
+			sql += "'" + day + "/" + month + "/2014');";
+
+			System.out.println(sql);
 		}
 
 	}
